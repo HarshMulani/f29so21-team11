c1caaf8dfd508fa75b7ee2c5ea673c4e9a388a26
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2021-12-26 02:32:29 +0000

    added sendMessage and changed room listening

--- client/src/app/services/room-manager/room-manager.service.ts
+++ client/src/app/services/room-manager/room-manager.service.ts
@@ -12,11 +12,23 @@ export class RoomManagerService {
   constructor(private socketMan: SocketManagerService) {  }
 
   createRoom() {
-    this.socketMan.emitEvent('createRoom', null);
+    this.socketMan.emitEvent('create-room', null);
   }
 
   subscribeToRoom() {
-    this.socketMan.subscribeToEvent('room-list-update', (rooms) => {console.log(rooms); this.rooms = rooms})
-    this.socketMan.emitEvent('get-room-list', null);
+    this.socketMan.subscribeToEvent('room-list-update', (rooms) => {this.rooms = rooms;})
+    this.socketMan.emitEvent('get-all-room', null);
   }
+
+  sendMessage(mVal : string,  id : string | null) {
+    let message = { roomid: id, message: mVal, sentBy: "User", timestamp: new Date() };
+    console.log("Message: ", message)
+    if (id == null) {return}
+    this.socketMan.emitEvent(id, message);
+  }
+
+  // listenToRoom(id:string) {
+  //   console.log(`Subscribing to Room with id: ${id}`)
+  // }
+
 }
