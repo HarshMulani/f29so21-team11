78b4f97734aa3415195dc2e79ea331a19498624f
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2021-12-29 01:55:47 +0000

    Moved account management to seperate file

--- server/src/app/server.ts
+++ server/src/app/server.ts
@@ -3,6 +3,7 @@ import { Connection } from "typeorm";
 import { User } from "../entities/User";
 import { RoomSocket } from "./room-socket";
 import { MessageSocket } from "./message-socket";
+import { UserSocket } from "./user-socket";
 
 
 var server = (io: Server, connection: Connection) => {
@@ -10,65 +11,23 @@ var server = (io: Server, connection: Connection) => {
     // const ws = new WhiteBoardSocket(io);
     const rs = new RoomSocket(io); // setting up functionality of rooms
     const ms = new MessageSocket(io, rs); // setting up functionality of messages
+    const us = new UserSocket(io, connection); // setting up functionality of user connections
 
     io.on('connection', (socket: Socket) => { // user connects
 
         console.log("User with id " + socket.id + " joined")
         
-
-/*  =======================================================================================================================================================================  */
-/*  ===============================================================  Chat Room Management  ================================================================================  */
-/*  =======================================================================================================================================================================  */
-
+        /* Room socket management */
         rs.setupEvents(socket); // making listeners for recieving room events
+        
+        /* message socket management */
         ms.setupEvents(socket); // making listeners for recieving chat message events
-
-
-/*  =======================================================================================================================================================================  */
-/*  =================================================================  Account Management  ================================================================================  */
-/*  =======================================================================================================================================================================  */
-
-
-        socket.on('account-created', async (acc) => {
-            let userRepo = connection.getRepository(User);
-            let user = new User();
-            user.Username = acc.username;
-            user.Password = acc.password;
-            user.Email = null;
-            await userRepo.save(user).then(() => { console.log("User saved to database") }).catch((err) => console.log(err));
-            console.log(`Account created with Username: ${acc.username} and Password: ${acc.password}`);
-        });
-
-        socket.on('login', async (account) => {
-            let userRepo = connection.getRepository(User);
-            let accUser = await userRepo.find({ Username: account.username });
-            if (accUser != null && accUser[0].Password == account.password) {
-                socket.emit('successfully-logged-in');
-            }
-        });
-
-        socket.on('clear-users', async () => {
-            let userRepo = connection.getRepository(User);
-            let usersToDelete = await userRepo.find();
-            usersToDelete.forEach((user) => { if (user.Username != socket.id || user.Username != 'TestUser') { userRepo.remove(user); } });
-
-            let Users = await userRepo.find();
-            Users.forEach((user) => { console.log(`User: ${user.Username}`); });
-        })
-
-
-/*  =======================================================================================================================================================================  */
-/*  ==================================================================  Socket Management  ================================================================================  */
-/*  =======================================================================================================================================================================  */
-
+        
+        /* user/account socket management */
+        us.setupEvents(socket); // making listeners for recieving user account events
 
         socket.on('disconnect', async (reason) => {
-            console.log(`User with id ${socket.id} has disconnected`);
-            let userRepo = connection.getRepository(User);
-            let userToLeave = await userRepo.findOne({ Username: socket.id }).catch((err) => { console.log(err); });
-            if (userToLeave) {
-                await userRepo.remove(userToLeave).catch((err) => { console.log(err); });
-            }
+            console.log(`User with id ${socket.id} has disconnected with reason: ${reason}`);
         })
     })
 }
