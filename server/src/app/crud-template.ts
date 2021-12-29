d383d8578393021c05ec425697c94b10fe235129
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2021-12-29 01:53:48 +0000

    Changed layout and comments

--- server/src/app/crud-template.ts
+++ server/src/app/crud-template.ts
@@ -15,6 +15,13 @@ export abstract class crudtemplate<T extends {id: string}> {
         this.io = io;
     }
 
+    setupEvents(socket: Socket) { // Setting up C, R, U, D listeners for suers socket
+        this.create(socket); // create brand new item
+        this.read(socket);   // returns all items
+        this.update(socket); // updates item that already exists
+        this.delete(socket); // delete item that exists
+    }
+    
     // all abstract functions, modified or used on extended classes
     // functions can be overwritten but don't have to be
     // this can never be a class but always must be extended
@@ -29,40 +36,32 @@ export abstract class crudtemplate<T extends {id: string}> {
         // use item from db await()
     }
 
-    setupEvents(socket: Socket) { // Setting up C, R, U, D listeners for suers socket
-        this.create(socket); // create brand new item
-        this.read(socket);   // returns all items
-        this.update(socket); // updates item that already exists
-        this.delete(socket); // delete item that exists
-    }
-
-
     emitUpdate(options?: {socket?: Socket, id?: string}) {
         throw new Error('Not implemented')
     }
 
 
     create(socket: Socket) {
-        socket.on(`create-${this.name}`, (data: T) => { // create Room
+        socket.on(`create-${this.name}`, (data: T) => { // create item
         this.makeItem(data)
         
         this.emitUpdate();
     })}
 
     read(socket: Socket) {
-        socket.on(`get-all-${this.name}`, () => { // Read Rooms
+        socket.on(`get-all-${this.name}`, () => { // Read items
             this.emitUpdate();
         })
     }
     
     update(socket: Socket) {
-        socket.on(`update-${this.name}`, (id : string) => { // Update Room
+        socket.on(`update-${this.name}`, (id : string) => { // Update item
             this.emitUpdate({socket, id});
         })
     }
     
     delete(socket: Socket) {
-        socket.on(`delete-${this.name}`, (id: string) => { // Delete Room
+        socket.on(`delete-${this.name}`, (id: string) => { // Delete item
             if (!this.itemExists(id)) return;
             this.emitUpdate();
         })
