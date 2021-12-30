3a89d67cd0a241296ef54145d9f329e2fcd5e8ea
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2021-12-30 00:23:43 +0000

    created group room

---
+++ client/src/app/componants/group-room/group-room.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-room',
+  templateUrl: './group-room.component.html',
+  styleUrls: ['./group-room.component.scss']
+})
+export class GroupRoomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
