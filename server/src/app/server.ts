1ff5c8efd7c9b942aecd5ccc975ca0fa6d0fded3
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2022-01-01 01:18:56 +0000

    added groupsockets to server

--- server/src/app/server.ts
+++ server/src/app/server.ts
@@ -1,14 +1,14 @@
 import { Server, Socket } from "socket.io";
 import { Connection } from "typeorm";
-import { User } from "../entities/User";
 import { RoomSocket } from "./room-socket";
 import { MessageSocket } from "./message-socket";
 import { UserSocket } from "./user-socket";
+import { GroupSockets } from "./group-socket";
 
 
 var server = (io: Server, connection: Connection) => {
 
-    // const ws = new WhiteBoardSocket(io);
+    const gs = new GroupSockets(io); // setting up functionality of groups
     const rs = new RoomSocket(io); // setting up functionality of rooms
     const ms = new MessageSocket(io, rs); // setting up functionality of messages
     const us = new UserSocket(io, connection); // setting up functionality of user connections
@@ -17,6 +17,10 @@ var server = (io: Server, connection: Connection) => {
 
         console.log("User with id " + socket.id + " joined")
         
+        // socket.on('whiteboard-draw', (actions: { x: number, y: number, size: number, color: {r: string, g: string, b: string}}) => {
+        //     io.emit('whiteboard-update', actions)
+        // })
+
         /* Room socket management */
         rs.setupEvents(socket); // making listeners for recieving room events
         
@@ -25,6 +29,9 @@ var server = (io: Server, connection: Connection) => {
         
         /* user/account socket management */
         us.setupEvents(socket); // making listeners for recieving user account events
+
+        /* group socket management */
+        gs.setupEvents(socket); // making listeners for recieving group events
 
         socket.on('disconnect', async (reason) => {
             console.log(`User with id ${socket.id} has disconnected with reason: ${reason}`);
