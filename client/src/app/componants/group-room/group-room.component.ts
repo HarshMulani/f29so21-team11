e1a4781813bcd4b5ea41140d96bb0c06ef946636
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2022-01-14 18:22:29 +0000

    added code to join a group-room

--- client/src/app/componants/group-room/group-room.component.ts
+++ client/src/app/componants/group-room/group-room.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { WhiteBoardRoom } from 'src/app/models/WhiteBoardRoom';
+import { GroupManagerService } from 'src/app/services/group-manager/group-manager.service';
+import { SocketManagerService } from 'src/app/services/socket-manager/socket-manager.service';
 
 @Component({
   selector: 'app-group-room',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupRoomComponent implements OnInit {
 
-  constructor() { }
+  constructor(private socketMan: SocketManagerService, private groupMan: GroupManagerService, private router: Router, private activeRoute: ActivatedRoute) { }
+
+  get rooms(): Array<WhiteBoardRoom> {
+    return this.groupMan.rooms;
+  }
+
+  createRoom() {
+    this.groupMan.createRoom();
+  }
+
+  joinRoom(id: string) {
+    this.router.navigate([id], { relativeTo: this.activeRoute });
+    this.socketMan.emitEvent('get-all-group', null);
+    console.log(this.rooms)
+    this.groupMan.listenToRoomMessage(id);
+    this.groupMan.listenToRoomWhiteboard(id);
+    let subscribedRoom = this.groupMan.subscribedRooms.find((room) => room.id == id);
+    if (subscribedRoom != undefined) return
+    this.groupMan.subscribedRooms.push({id: id, val: true});
+    this.socketMan.emitEvent('update-group', {roomId: id});
+    this.socketMan.emitEvent('update-message', id);
+  }
 
   ngOnInit(): void {
+    this.groupMan.subscribeToRoom();
   }
 
 }
