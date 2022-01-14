5992afaff9925dc947160ba6bb86ce8a5c82b491
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2022-01-14 18:27:03 +0000

    whiteboard manager to handle whiteboard in future

--- client/src/app/services/whiteboard-manager/whiteboard-manager.service.ts
+++ client/src/app/services/whiteboard-manager/whiteboard-manager.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Color } from 'src/app/models/Color';
+import { WhiteBoardRoom } from 'src/app/models/WhiteBoardRoom';
+import { GroupManagerService } from '../group-manager/group-manager.service';
+import { RoomManagerService } from '../room-manager/room-manager.service';
+import { SocketManagerService } from '../socket-manager/socket-manager.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WhiteboardManagerService {
 
-  constructor() { }
+  canvasList: Array<{id: string, canvas: HTMLElement | null}> = []
+
+  constructor(private socketMan: SocketManagerService, private activeRoute: ActivatedRoute, private groupMan:  GroupManagerService) { }
+
+
+
 }
