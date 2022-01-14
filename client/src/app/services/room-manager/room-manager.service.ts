ddca936d6bffc182d4f8c8d137a83b77ec884df8
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2022-01-14 18:26:23 +0000

    removed debug code

--- client/src/app/services/room-manager/room-manager.service.ts
+++ client/src/app/services/room-manager/room-manager.service.ts
@@ -25,16 +25,13 @@ export class RoomManagerService {
 
   sendMessage(mVal : string,  id : string | null) {
     let message = { roomid: id, message: mVal, sentBy: "User", timestamp: new Date() };
-    console.log("Message: ", message)
     if (id == null) {return}
     this.socketMan.emitEvent(id, message);
   }
 
   listenToRoom(id: string) {
-    console.log("subscribing to room")
     this.socketMan.subscribeToEvent(`message-update-${id}`, (msg : Message) => {
       let currRoom = this.rooms.find((room) => room.id == id);
-      console.log(`Recieving message for room with id: ${id}`);
       if (currRoom?.history.find((message) => message == msg)) return
       currRoom?.history.push(msg);
     });
