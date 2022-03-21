1298a97c25d6f2bf4a77ddeaf7e402b9e2bfe47d
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2022-03-21 21:06:12 +0000

    Update User.ts

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -4,14 +4,14 @@ import { Entity, Column, PrimaryColumn, Generated } from 'typeorm';
 export class User {
 
     @Column({ length: 40 })
-    ID: string;
+    ID!: string;
 
     @PrimaryColumn({ length: 25 })
-    Username: string;
+    Username!: string;
 
     @Column({ length: 100 })
-    Password: string;
+    Password!: string;
 
     @Column({ length: 50, nullable: true, })
-    Email: string;
+    Email!: string;
 }
