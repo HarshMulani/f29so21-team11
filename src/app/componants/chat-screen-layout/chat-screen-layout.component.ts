0798d86ced40de22d0fb62871c61079c05e5ac02
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2022-03-28 20:51:38 +0100

    updated chat screen layout componant

--- src/app/componants/chat-screen-layout/chat-screen-layout.component.ts
+++ src/app/componants/chat-screen-layout/chat-screen-layout.component.ts
@@ -10,6 +10,25 @@ export class ChatScreenLayoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let date = localStorage.getItem('auth-token')
+    let currDate = new Date().getTime().toString();
+    
+    console.log(date, currDate)
+    if (date) {
+      if (date > currDate) {
+        let elems = document.getElementsByClassName('to-hide')
+
+        for (let i = 0; i < elems.length; i++) {
+          elems[i].classList.remove('hidden')
+        }
+        return;
+      }
+    }
+    let elems = document.getElementsByClassName('to-hide')
+
+    for (let i = 0; i < elems.length; i++) {
+      elems[i].classList.add('hidden')
+    }
   }
 
 }
