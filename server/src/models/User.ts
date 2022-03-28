ed589741df4aa891960701a79ddfc372a083542d
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2022-03-28 20:47:46 +0100

    updated server models

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -3,4 +3,6 @@ export interface User {
     username: string;
     password: string;
     email: string;
+    stats: {posts: number, groups: number, chats: number};
+    bio: string;
 }
--- server/src/models/WhiteboardRoom.ts
+++ server/src/models/WhiteboardRoom.ts
@@ -1,8 +1,11 @@
+import { User } from "./User";
 import { Room } from "./Room";
 
 export interface WhiteBoardRoom {
     id: string;
     name: string;
+    manager: string;
     whiteboard: string;
     chat: Room;
+    participants: Array<string>;
 }
