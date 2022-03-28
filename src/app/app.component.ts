07d4cad39f6ab9523d9e5b93976259ff1f4b136a
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2022-03-28 21:06:46 +0100

    updated nav-bar componant script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { SocketManagerService } from './services/socket-manager/socket-manager.service';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +10,30 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class AppComponent implements AfterViewInit {
   title = 'f29so-Project';
 
-  tab = ['', 'company', 'home', 'chat', 'whiteboard', 'login']
+  tab = ['', 'company', 'home', 'chat', 'whiteboard', 'account']
 
   currentMode = -1;
 
-  constructor(private router: Router, private activeRoute: ActivatedRoute) { this.changeMode() }
+  constructor(private socketMan: SocketManagerService, private router: Router, private activeRoute: ActivatedRoute) { this.currentMode = -1; this.changeMode() }
 
 
   ngAfterViewInit(): void {
-    this.activeLink(this.tab.indexOf('login'));
+
+    // setTimeout(() => {
+    //   if (localStorage.getItem('login-token')) {
+    //     this.socketMan.emitEvent('get-personal-user', localStorage.getItem('login-token'))
+    //   }
+    // }, 10000);
+
+    if (!localStorage.getItem('auth-token')) {
+      console.log(this.router.url)
+      // this.router.navigate(['/account'], { relativeTo: this.activeRoute });
+      this.router.navigateByUrl('/account/login')
+      this.activeLink(5)
+    }
+
+    this.activeLink(this.tab.indexOf(window.location.href.split('/')[3]));
+
   }
 
   activeLink(id: number) {
@@ -35,8 +51,8 @@ export class AppComponent implements AfterViewInit {
       document.documentElement.style.setProperty('--bgc', '#3a445d');
       document.documentElement.style.setProperty('--cbgc', '#5e5768');
     } else {
-      document.documentElement.style.setProperty('--bgc', 'beige');
-      document.documentElement.style.setProperty('--cbgc', '#a8f0e6');
+      document.documentElement.style.setProperty('--bgc', '#F4FFF8');
+      document.documentElement.style.setProperty('--cbgc', '#FFE499');
     }
 
     this.currentMode *= -1;
