6e34db711ec5b9d0682ebed23c75ed193c05ccc0
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2022-03-28 20:51:50 +0100

    updated chat window componant

--- src/app/componants/chat-window/chat-window.component.ts
+++ src/app/componants/chat-window/chat-window.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Room } from 'server/src/models/Room';
 import { Message } from 'src/app/models/Message';
+import { GroupManagerService } from 'src/app/services/group-manager/group-manager.service';
 import { RoomManagerService } from 'src/app/services/room-manager/room-manager.service';
 import { SocketManagerService } from 'src/app/services/socket-manager/socket-manager.service';
 
@@ -16,15 +18,27 @@ export class ChatWindowComponent implements OnInit, OnDestroy {
 
   subscribed: { [key: string]: boolean } = {};
 
-  constructor(private activeRoute: ActivatedRoute, private roomMan: RoomManagerService, private socketMan: SocketManagerService) { }
+  constructor(private router: Router, private roomMan: RoomManagerService, private groupMan: GroupManagerService, private socketMan: SocketManagerService) { }
   ngOnDestroy(): void {
     // this.historyService.messages = [];
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    let messageHist = document.getElementById('messageHist');
+    setTimeout(() => {
+      messageHist?.scrollTo(0, messageHist.scrollHeight);
+    }, 100);
+  }
 
+  get manager(): boolean {
+    return (this.room.manager == localStorage.getItem('login-token')) ? true : false;
+  }
   get currentRoomId() : string | null {
-    return this.activeRoute.snapshot.params['id'];
+    return this.router.url.split('/')[2]
+  }
+
+  get room() : Room {
+    return this.roomMan.rooms.find((room) => room.id == this.currentRoomId)!
   }
 
   get messages(): Array<Message> {
@@ -33,15 +47,31 @@ export class ChatWindowComponent implements OnInit, OnDestroy {
     return (currRoom == undefined) ? [] : currRoom.history;
   }
 
-  sendMessage(mVal: string) {
-    if (mVal == '') { return }
+  sendMessage() {
+    let mVal = document.getElementById('msgInput') as HTMLInputElement
+    if (mVal.value == '') { return }
     let messageHist = document.getElementById('messageHist');
-    messageHist?.scrollTo(0, messageHist.scrollHeight);
-    this.roomMan.sendMessage(mVal, this.currentRoomId)
+    this.roomMan.sendMessage(mVal.value, this.currentRoomId)
+    mVal.value = ''
+    setTimeout(() => {
+      messageHist?.scrollTo(0, messageHist.scrollHeight);
+    }, 10);
   }
 
   unsubscribeFromRoom() {
     this.socketMan.sock.removeListener(`message-update-${this.roomMan.a[this.currentRoomId!]}`);
     this.roomMan.a[this.currentRoomId!] = null;
+  }
+
+  changeRoomName() {
+    let newName = prompt("Enter new room name")
+    let room = this.room
+    room.name = newName!
+    this.socketMan.emitEvent('update-room', room)
+    let group = this.groupMan.rooms.find(group => group.id == this.currentRoomId)
+    if (group) {
+      group.name = newName!
+      this.socketMan.emitEvent('update-group-room', group)
+    }
   }
 }
